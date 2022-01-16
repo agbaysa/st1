efb76b3c98429b5394dc13692568d9096a3b1926
agbaysa <33347932+agbaysa@users.noreply.github.com>
2022-01-16 15:36:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 import streamlit as st
+import time
 
 import warnings
 warnings.filterwarnings('ignore')
