1fda7414b6231c2af40ca7ded5ee8a139950c699
agbaysa <33347932+agbaysa@users.noreply.github.com>
2022-01-16 15:39:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ if st.button('Submit'):
 
     # Progress:
     progress = st.progress(0)
-    for i in range(20):
-        time.sleep(0.01)
+    for i in range(100):
+        time.sleep(0.001)
         progress.progress(i+1)
 	
     
